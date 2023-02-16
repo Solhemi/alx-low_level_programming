ae5b8487026ae7fe16ba95331b519ab7d56e609d
Solen Akenna <solenbg@gmail.com>
2023-02-16 15:25:37 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,14 +13,11 @@ int main(void)
 	long int y;
 	long long int z;
 	float u;
-
-	
 printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(w));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(x));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of(y));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of(z));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(u));
-
-return (0)
+return (0);
 
 }
