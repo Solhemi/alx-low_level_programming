1abdb176675595e5d0f1a6af1ac17d36ac7135bf
Solen Akenna <solenbg@gmail.com>
2023-02-19 12:09:07 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: always (0) success
+ */
+
+int main(void)
+{
+char num = '0';
+for (num = '0';num <= '9'; num++)
+putchar(num);
+putchar('\n');
+return (0);
+}
