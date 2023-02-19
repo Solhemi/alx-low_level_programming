3cdfed50f14e1cedd4f26a945beba1826a7a3155
Solen Akenna <solenbg@gmail.com>
2023-02-19 14:21:52 -0800

    101 quote c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main- entry point
+ *
+ *
+ * return: always (0) success
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful" - Dora Korpar, 2015-10-19", stderr);
+	return (0);
+}
