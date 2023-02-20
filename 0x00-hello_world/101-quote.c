4d88db58e955ae8a1b7032535bf365e6e243103b
Solen Akenna <solenbg@gmail.com>
2023-02-20 14:08:18 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,13 @@
 
 /**
  * main- entry point
+ * main is a program to print string to standard error
  *
- *
- * return: always (0) success
+ * return: always (1) success
  */
 
 int main(void)
 {
-	fwrite(""and that piece of art is useful" - Dora Korpar, 2015-10-19", stderr);
-	return (0);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
