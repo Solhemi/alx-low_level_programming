d255758f4f83771bc2317890bd71bc105e83000d
Solen Akenna <solenbg@gmail.com>
2023-02-21 13:13:56 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * Main - entry point
+ *
+ * Description - checks the number
+ *
+ * return: 1 for positive, -1 for negative and zero for otherwise
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
