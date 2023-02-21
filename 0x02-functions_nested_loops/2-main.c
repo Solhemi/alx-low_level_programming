5752150b6e50f61af06488432b06cd7b789309c6
Solen Akenna <solenbg@gmail.com>
2023-02-21 11:46:32 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main-entry point
+ *
+ * Description - function prints the alphabet ten times
+ *
+ * return: always 0 success
+ */
+
+int main(void)
+
+{
+	print_alphabet_x10();
+	return (0);
+}
