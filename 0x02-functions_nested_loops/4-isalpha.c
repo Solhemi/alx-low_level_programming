811c3ef817ee3cf2cc8262ab45879cd6be46c88a
Solen Akenna <solenbg@gmail.com>
2023-02-21 12:38:34 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Main - entry point
+ *
+ * Description: checks for alphabet characters
+ *
+ * return: 1 for success 0 otherwise
+ */
+
+int _isalpha(int c);
+
+{
+	if ((c >= 65; && c <= 90) || (c >= 97; && c <= 122))
+	{
+		return (1);
+	}
+
+	{
+		return (0);
+	}
+}
