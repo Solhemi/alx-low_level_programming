25a57d8714ce87c6aaff0108cf57dba50e03c084
Solen Akenna <solenbg@gmail.com>
2023-02-22 09:00:02 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main -entry point
+ *
+ * Description - function prints last digit of a number
+ *
+ * return value of last digit
+ */
+
+int print_last_digit(int n)
+
+{
+	int last = ((n % 10));
+
+	if (last > 0)
+	{
+		_putchar(last);
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (last);
+}
