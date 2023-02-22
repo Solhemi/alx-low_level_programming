6a06adf35cdf575db958d76b7f4859f60c96c9b1
Solen Akenna <solenbg@gmail.com>
2023-02-22 09:11:40 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,13 +13,10 @@ int print_last_digit(int n)
 {
 	int last = ((n % 10));
 
-	if ((last > 0) || (last < 0))
+	if (last < 0)
 	{
-		_putchar(last);
+		last = last * -1;
 	}
-	else
-	{
-		_putchar('0');
-	}
+	_putchar(last +'0');
 	return (last);
 }
