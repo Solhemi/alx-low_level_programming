b35572a845342d4ffe50a819d771eab434425945
Solen Akenna <solenbg@gmail.com>
2023-02-22 09:17:45 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,21 +2,23 @@
 
 /**
  * main -entry point
- *
+ * n is the number to be tested
  * Description - function prints last digit of a number
  *
- * return value of last digit
+ * return  1 success
  */
 
 int print_last_digit(int n)
 
 {
+	int last;
+
 	int last = ((n % 10));
 
 	if (last < 0)
 	{
 		last = last * -1;
 	}
-	_putchar(last +'0');
+	_putchar(last + '0');
 	return (last);
 }
