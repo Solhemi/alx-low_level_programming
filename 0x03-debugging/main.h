c162950ed56ce2cc6ab347cd269a33f62e8eaee7
Solen Akenna <solenbg@gmail.com>
2023-02-23 12:15:24 -0800

    multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,17 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - checks the number
+ * largest_number - picks largest of all
+ * convert_day - converts dates
+ * @i: integer
+ * print_remaining_days - prints days left
+ */
+
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
-print_remaining_days(month, day, year);
-int convert_day(int month, int day)
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
