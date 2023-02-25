ee777f203038cb45eb897d73aa7f9af65ba89949
Solen Akenna <solenbg@gmail.com>
2023-02-25 02:31:34 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - tests if char. is upper case
+ * @c: is an integer input
+ * Dexcription:_isupper function
+ * Return: (1) success
+ */
+
+int _isupper(int c);
+
+{
+	int c;
+
+	if ((c >= 65) && (c <= 90))
+	{
+	_putchar('49');
+	_putchar("\n");
+	}
+	else
+	{
+		_putchar('48');
+	}
+	return (1);
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+0x04. C - More functions, more nested loops
+1. isupper
+2. isdigit
+3. Collaboration is multiplication and  others
