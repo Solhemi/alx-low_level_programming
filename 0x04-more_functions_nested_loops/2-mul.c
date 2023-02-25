8ec776fe42106f23537dbde90a3fe76e3e594795
Solen Akenna <solenbg@gmail.com>
2023-02-25 06:32:24 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ * @a: int a
+ * @b: int b
+ * @x: int x
+ * Return: success (x)
+ */
+
+int mul(int a, int b)
+{
+	int x;
+	x  = a * b;
+		return (x);
+}
