81156cd31d244e34763e2a7dccf88013edeafab6
Solen Akenna <solenbg@gmail.com>
2023-02-25 06:10:52 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - fn
+ *
+ * @c: is int
+ * Description: test digit
+ * Return: 1 is success 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	for (d >= 0 && d <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
