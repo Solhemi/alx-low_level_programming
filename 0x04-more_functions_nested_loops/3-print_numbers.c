9b7fef63dd61823104afae353c4731688f463629
Solen Akenna <solenbg@gmail.com>
2023-02-25 06:48:28 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints no.
+ *
+ * Description: prints 0-9
+ * Return: success (0)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i >= 48; i <= 57; i++)
+	{
+		_putchar(i);
+		_putchar("\n");
+	}
+	return (0);
+}
