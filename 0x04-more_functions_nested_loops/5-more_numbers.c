c825fe4eed54ed492a903e26bf18bf4d0ea40997
Solen Akenna <solenbg@gmail.com>
2023-02-26 01:38:31 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - zero to 14
+ *
+ * Description: prints row 3 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void);
+{
+	int x, y;
+
+	if (y >= 0 && y <= 3)
+	{
+		for (x >= 0; x <= 14; x++)
+		{
+			_putchar(X + '0');
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
