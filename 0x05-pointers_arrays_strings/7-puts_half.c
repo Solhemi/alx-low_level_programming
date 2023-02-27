759ac10deac4b5ffde4994dc9a2313b035c198f6
Solen Akenna <solenbg@gmail.com>
2023-02-27 11:54:38 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,34 @@
 #include "main.h"
 
 /**
+ * puts_half - prt 2nd half
+ * @str: string
  *
- *
- *
- *
+ * Return: void
  */
 
 void puts_half(char *str)
+
+{
+	int l = 0;
+	int m;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if (l % 2 == 1)
+	{
+		m = (l - 1) / 2;
+		m = m + 1;
+	}
+	else
+	{
+		m = l / 2;
+	}
+	for (; m < l; m++)
+	{
+		_putchar(str[m]);
+	}
+	_putchar('\n');
+}
