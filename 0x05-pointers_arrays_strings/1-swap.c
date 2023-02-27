a7eb56b21bf81d355099771659f4ea22330ebe12
Solen Akenna <solenbg@gmail.com>
2023-02-27 06:59:18 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,14 @@
 
 /**
  * swap_int - swaps int
- *
- *
+ * @a: int
+ * @b: int
  *
  * Return: void
  */
 
 void swap_int(int *a, int *b)
 {
-	a = a - b;
-	b = a + b;
-	a = b - a;
+	*a = b;
+	*b = a;
 }
