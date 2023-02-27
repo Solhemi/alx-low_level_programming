e7598d046bccbedc48ae5b3c3d89c4679bc45ea7
Solen Akenna <solenbg@gmail.com>
2023-02-27 06:19:06 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - rests n to 98
+ * 
+ *
+ * Return: 98
+ */
+
+void reset_to_98(int *n)
+
+{
+	int n, p;
+
+	n = 97;
+	*p = &n;
+
+	p = 98;
+	_putchar((i / 10) + '0');
+	_putchar((i % 10) + '0');
+	_putchar('\n');
+	return (98);
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+0x05. C - Pointers, arrays and strings
+1. 98 Battery st
+2. Don't swap horses in crossing a stream
+3. This report, by its very length, defends itself against the risk of being read
+ and others
