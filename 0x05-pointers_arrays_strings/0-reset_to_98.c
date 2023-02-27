428d0a7e2e152e253bb099f62386ff804b45ac73
Solen Akenna <solenbg@gmail.com>
2023-02-27 06:41:46 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,23 +2,13 @@
 
 /**
  * reset_to_98 - rests n to 98
- * 
+ * @n: integer
  *
- * Return: 98
+ * Return: void
  */
 
 void reset_to_98(int *n)
 
 {
-	int n, p;
-
-	n = 97;
-	*p = &n;
-
-	p = 98;
-	_putchar((i / 10) + '0');
-	_putchar((i % 10) + '0');
-	_putchar('\n');
-	return (98);
+	*n = 98;
 }
-
