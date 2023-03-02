703b869fa77e1efb8062cdfaf70f06030d5f42f3
Solen Akenna <solenbg@gmail.com>
2023-03-02 13:48:36 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *leet - fn
+ *
+ *
+ *
+ *
+ *Return: chari
+ */
+
+char *leet(char *n)
+
+{
+	int l, m;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (l = 0; n[l] != '\0'; l++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			if (n[l] = s1[m])
+			{
+				n[l] = s2[m];
+			}
+		}
+	}
+	return (n);
+}
