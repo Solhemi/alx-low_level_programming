2fa2adc3add889b15db9e266ddd2802829155302
Solen Akenna <solenbg@gmail.com>
2023-03-02 05:38:50 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,27 @@
 #include <main.h>
 
 /**
- *
- *
- *
- *
- *
+ * _strncat - concat str.
+ * @dest: destn
+ * @src: source
+ * @n: int
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
+
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		for (j = 0; j < n; src != '\0')
+		{
+			i++;
+			j++;
+			dest[i] = src[j];
+		}
+	}
+	dest[i] = '\0';
+	return (dest);
+}
