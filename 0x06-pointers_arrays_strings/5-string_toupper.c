90e5b13d66f042f20c496b6343a7802a50c501b9
Solen Akenna <solenbg@gmail.com>
2023-03-02 11:29:44 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - f
+ * 'a': char
+ * 'n': array
+ * 'z': char
+ * @ ' ': char
+ * Return: n
+ */
+
+char *string_toupper(char *)
+
+{
+	int j;
+
+	j = 0;
+	while (n[j] != '\0')
+	{
+		if (n[j] >= 'a' && n[j] <= 'z')
+			n[j] = n[j] - 32;
+		j++
+	}
+	return (n);
+}
