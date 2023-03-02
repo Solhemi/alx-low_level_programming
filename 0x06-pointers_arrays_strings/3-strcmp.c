d7c2e0a90cf3d381f51ed783a3b365af6bbde790
Solen Akenna <solenbg@gmail.com>
2023-03-02 10:19:19 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,22 +11,16 @@
 int _strcmp(char *s1, char *s2)
 
 {
-	int i, j;
+	int i = 0;
 
-	while (i = 0 && s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] '\0')
 	{
-		i++;
-	}
-	while (j = 0 && s2[j] != '\0')
-	{
-		j++;
-		if (s1[i] != s2[j])
+		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[j]);
-		}
-		else
-		{
-			return (0);
-		}
-	}
-}
+			return (s1[i] - s2[i]);
+					}
+					i++;
+					}
+					return (0);
+					}
+
