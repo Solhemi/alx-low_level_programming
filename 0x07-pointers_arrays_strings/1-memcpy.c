10ce4215052df2bcb8166df92ccc23932ee78336
Solen Akenna <solenbg@gmail.com>
2023-03-06 13:22:57 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
 /**
+ * *_memcpy - copy fn
+ * @src: source
+ * @dest: destination
+ * @n: int
  *
- *
- *
- *
- *
- *
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
+	int i;
+	int n;
+
+	for (i = 0; n > 0; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
