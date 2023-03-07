82000b4804b625e0d74e25c2c3548bf1105bfe2b
Solen Akenna <solenbg@gmail.com>
2023-03-07 11:12:31 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - fn
+ * @accept: inp
+ * @s: inp
+ *
+ * Return: 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+	unsigned int n = 0;
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+			{
+				n++;
+				break;
+			}
+			else if (accept[b + 1] == '\0')
+
+				return (n);
+		}
+		s++
+	}
+	return (n);
+}
+
