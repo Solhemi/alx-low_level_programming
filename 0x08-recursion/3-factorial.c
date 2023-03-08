690e385453a570358ae6b15003af0c30a54e5dec
Solen Akenna <solenbg@gmail.com>
2023-03-08 14:04:56 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - fn
+ * @n: int
+ *
+ * Return: 0
+ */
+
+int factorial(int n)
+
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+
+		return (n * factorial(n - 1));
+}
