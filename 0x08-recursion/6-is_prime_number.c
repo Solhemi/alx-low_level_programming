71d9cdc9aa8e0eb73e7a5ea800e03e8b19982e25
Solen Akenna <solenbg@gmail.com>
2023-03-09 11:32:40 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 
 /**
- * _prime_number - fn
+ * check_prime - fn
  * @n: number
  * @i: int
  *
  * Return: 1
  */
 
-int _prime_number(int n, int i);
+int check_prime(int n, int i);
 
 int is_prime_number(int n)
 {
-	return (_prime_number(n, 1));
+	return (check_prime(n, 1));
 }
 /**
- * is_prime_number - fn
+ * check_prime - fn
  * @n: number
  * @i: int
  *
  * Return: 0
  */
-int _prime_number(int n, int i)
+int check_prime(int n, int i)
 {
 	if ((n <= 1)
 		return (0);
@@ -32,5 +32,5 @@ int _prime_number(int n, int i)
 
 	if ((n / i) < i)
 		return (1);
-		return (_prime_number(n, i + 1));
+		return (check_prime(n, i + 1));
 }
