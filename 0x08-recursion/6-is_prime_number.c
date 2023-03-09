a1bf9fcf24abd72d6c9469f659db468dc1e739ac
Solen Akenna <solenbg@gmail.com>
2023-03-09 11:22:25 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,18 +11,18 @@
 
 int is_prime_number(int n, int i);
 
-int is_prime_number(int n)
+int _prime_number(int n)
 {
-	return (is_prime_number(n, 1));
+	return (_prime_number(n, 1));
 }
 /**
- * is_prime_number - fn
+ * _prime_number - fn
  * @n: number
  * @i: int
  *
  * Return: 0
  */
-int is_prime_number(int n, int i)
+int _prime_number(int n, int i)
 {
 	if ((n <= 1)
 		return (0);
@@ -32,5 +32,5 @@ int is_prime_number(int n, int i)
 
 	if ((n / i) < i)
 		return (1);
-		return (is_prime_number(n, i + 1));
+		return (_prime_number(n, i + 1));
 }
