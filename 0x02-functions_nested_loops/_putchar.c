c8a39e7148546eabf321d21157215ac704f49c2b
Solen Akenna <solenbg@gmail.com>
2023-03-10 13:53:50 -0800

    _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar -writes the character _putchar to standard error
- *
- * return: 1 (sucess)
+ * _putchar - writes the character _putchar to standard error
+ * @c: char
+ * Return: 1 (sucess)
  * on error -1 returned and error no set
  */
 int _putchar(char c)
