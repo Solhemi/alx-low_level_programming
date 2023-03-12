5453d4e4242094ec0565ef8596b9786d62704e13
Solen Akenna <solenbg@gmail.com>
2023-03-12 13:44:54 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - CLI fns
+ * @argc: arg count
+ * @argv: arg array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+
+{
+	int a, b, add;
+
+	a = 0;
+	b = 0;
+	if (argc == 3)
+	{
+		a = isdigit(atoi(argv[1]));
+		b = isdigit(atoi(argv[2]));
+		add = a + b;
+		printf("%d\n", add);
+	}
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
