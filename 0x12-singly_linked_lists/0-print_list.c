ee3c17ee3bb5dfc59a5ce188cec1a4c59d9d6d71
Solen Akenna <solenbg@gmail.com>
2023-04-01 06:56:25 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h)
+{
+	char str = 0;
+	
+	if (h->str == NULL)
+		printf("[0] nil\n");
+	else
+		printf("%u %s\n", h->len, h->str);
+	h = h->next;
+	str++;
+
+	return (str);
+}
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,7 @@
+0x12. C - Singly linked lists
+
+1. Print list
+2. List length
+3. Add node
+4. Add node at the end
+5. Free list and others
