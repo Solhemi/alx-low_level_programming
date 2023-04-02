c461e50e786547e8d67f889a9cecd132d64ba17a
Solen Akenna <solenbg@gmail.com>
2023-04-02 12:20:26 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,9 +4,9 @@
 #include <string.h>
 
 /**
- * print_list - fn to print all elements of struct list_t
+ * print_list - fn to print elements of struct list_t
  * @node: no of nodes
- * 
+ * @h: ptr to list_t
  *
  * Return: node no.
  */
@@ -15,11 +15,10 @@ size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 
-	while(h)
+	while (h)
 	{
 		if (h->str == NULL)
 		printf("[0] nil\n");
-	
 		else
 		printf("%u %s\n", h->len, h->str);
 		h = h->next;
