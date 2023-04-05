1bbf7009572cff0eb30548b665fa9373aad4b3aa
Solen Akenna <solenbg@gmail.com>
2023-04-05 03:19:02 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * list_len - length of list
+ * 
+ *
+ *
+ * return: i
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 1;
+
+	while (h)
+	{
+		if (h->next != NULL)
+			i++;
+	}
+	return (i);
+}
+
